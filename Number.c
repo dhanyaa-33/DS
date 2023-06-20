37b2f5b0029c28701a223a8c029a995652d4bd94
Dhanyaa <122244594+dhanyaa-33@users.noreply.github.com>
2023-06-20 14:18:59 +0530

    Create Number.c

---
+++ Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+int main()
+
+{
+
+int n,i;
+
+printf("Enter the stop number: ");
+
+scanf("%d",&n);
+
+for(i=1;i<=n;i++)
+
+printf("%d ",i);
+
+getch();
+
+return 0;
+
+}
